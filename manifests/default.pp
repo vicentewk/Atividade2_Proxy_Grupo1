af737a3e21ebe557bf9a84a16b96e6f41dc1be9f
Vicente Kryszczun <vicentewk@gmail.com>
2020-05-29 19:47:48 -0300

    upload dos arquivos

--- manifests/default.pp
+++ manifests/default.pp
@@ -12,10 +12,24 @@ package { [
   ensure  => 'installed',
 }
 	
+#faz a reconfiguração do squid após a alteração no squid.conf
+exec { "reconfigure_squid":
+	command => "sudo squid3 -k reconfigure",
+	require => Class["squid3"],
+  before => Exec ["restart_squid"],
+}
 
+#reinicializa o serviço após a reconfiguração do squid
+exec { "restart_squid":
+	command => "sudo service squid3 restart",
+  require => Class["arquivos"],
+  subscribe => Exec["reconfigure_squid"], #só executa após o reconfigure
+}
 
 include system-update
 include nginx
+include squid3
 include arquivos
+
 
 
--- modules/arquivos/manifests/init.pp
+++ modules/arquivos/manifests/init.pp
@@ -2,7 +2,7 @@ class arquivos {
 
 
 
-
+#criação do diretório imagens
 file {'/usr/share/nginx/www/imagens':
 	     ensure => directory,
        require => Class["nginx"],
@@ -10,7 +10,7 @@ file {'/usr/share/nginx/www/imagens':
 	     group  => 'root',
 	     mode   => '0644';
 	   } 
-
+#criação do diretório css
      file {'/usr/share/nginx/www/css':
 	     ensure => directory,
        require => Class["nginx"],
@@ -21,19 +21,19 @@ file {'/usr/share/nginx/www/imagens':
 
 
 
-
+#criação do arquivo index html
 file {'/usr/share/nginx/www/index.html':
      ensure => file,
      require => Class["nginx"],
      owner  => 'root',
      group  => 'root',
      mode   => '0644',
-     source => 'puppet:///modules/arquivos/fontehtml.txt',
+     source => 'puppet:///modules/arquivos/html/fontehtml.txt',
    } 
 
 
 
-
+#criação do arquivo de estilos css
    file {'/usr/share/nginx/www/css/estilo.css':
      ensure => file,
      require => Class["nginx"],
@@ -43,7 +43,7 @@ file {'/usr/share/nginx/www/index.html':
      source => 'puppet:///modules/arquivos/css/fontecss.txt',
    } 
 
-
+#criação dos arquivos de imagem
 file {'/usr/share/nginx/www/imagens/user.png':
      ensure => file,
      require => Class["nginx"],
@@ -80,6 +80,77 @@ file {'/usr/share/nginx/www/imagens/user.png':
      source => 'puppet:///modules/arquivos/imagens/arvore.png',
    } 
 
+    file {'/usr/share/nginx/www/imagens/proxy.png':
+     ensure => file,
+     require => Class["nginx"],
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0644',
+     source => 'puppet:///modules/arquivos/imagens/proxy.png',
+   } 
+
+    file {'/usr/share/nginx/www/imagens/squid.png':
+     ensure => file,
+     require => Class["nginx"],
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0644',
+     source => 'puppet:///modules/arquivos/imagens/squid.png',
+   } 
+
+    file {'/usr/share/nginx/www/imagens/squid_banner.jpg':
+     ensure => file,
+     require => Class["nginx"],
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0644',
+     source => 'puppet:///modules/arquivos/imagens/squid_banner.jpg',
+   } 
+#fim da criação dos arquivos de imagem
 		
 
+
+
+#criação dos arquivos do squid
+#comando para criar bakcup do arquivo squid.conf original
+    exec { "bkp_squid.conf_orig":
+	          command => "sudo cp /etc/squid3/squid.conf /etc/squid3/squid.conf.orig",
+	          require => Class["squid3"], 
+          }
+
+#arquivo squid.conf, responsável pela configuração do squid.
+#cria somente se o arquivo squid.conf.orig já tiver sido criado
+file {'/etc/squid3/squid.conf':
+     ensure => file,
+     require => Class["squid3"],
+     subscribe => Exec["bkp_squid.conf_orig"],
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0644',
+     source => 'puppet:///modules/arquivos/squid3/conf.txt',
+     
+   } 
+#arquivo contendo a lista de sites banidos
+   file {'/etc/squid3/ban.acl':
+     ensure => file,
+     require => Class["squid3"],
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0644',
+     source => 'puppet:///modules/arquivos/squid3/ban.txt',
+   } 
+
+#arquivo contendo a lista de sites banidos somente em um determinado período
+   file {'/etc/squid3/ban_tempo.acl':
+     ensure => file,
+     require => Class["squid3"],
+     owner  => 'root',
+     group  => 'root',
+     mode   => '0644',
+     source => 'puppet:///modules/arquivos/squid3/ban_tempo.txt',
+   } 
+
+   
 }
+
+
